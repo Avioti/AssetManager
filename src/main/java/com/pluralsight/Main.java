91233600c18b71b1e02dd96b9402eb75b3c33fff
Avi <95044844+Avioti@users.noreply.github.com>
2025-10-28 14:04:29 -0400

    finished

---
+++ src/main/java/com/pluralsight/Main.java
@@ -0,0 +1,82 @@
+package com.pluralsight;
+
+import com.pluralsight.models.*;
+import com.pluralsight.ui.Asset;
+
+import java.util.ArrayList;
+
+public class Main {
+    private static ArrayList<Asset> models = new ArrayList<>();
+    private static String message;
+    private static int counter;
+
+    public static void main(String[] args) {
+
+        Asset newHouse = new House("Dream house", "today", 300000, "123 main st", 5, 2500, 6000);
+        Asset newVehicle = new Vehicle("My car", "last month", 40000, 30000, 2, "Toyota");
+        models.add(newHouse);
+        models.add(newVehicle);
+
+
+        models.add(new House("MY HOUSE", "yesterday", 5, "over there", 5, 42, 59));
+        models.add(new House("Beach house", "last week", 250000, "beach ave", 4, 2000, 5000));
+        models.add(new Vehicle("Toms truck", "tuesday", 5, 53, 2300, "Tesla"));
+        models.add(new Vehicle("My car", "today", 50000, 120000, 5, "Honda"));
+        counter = 1;
+
+        //Example 1
+        for (Asset asset : models) {
+            counter++;
+
+            if (asset instanceof House) {
+                message = counter + " " + asset.getDescription() + " is located at " + ((House) asset).getAddress();
+            } else if (asset instanceof Vehicle) {
+                message = counter + " " + "This Vehicle is a " + ((Vehicle) asset).getMakeModel() + " and it is worth " + asset.getValue();
+            }
+            System.out.println(message);
+
+        }
+
+        //Example 2
+        for (int i = 0; i < models.size(); i++) {
+            counter++;
+            if (models.get(i) instanceof House) {
+                House house = (House) models.get(i);
+                message = counter + " " + house.getDescription() + " is located at " + house.getAddress();
+
+            } else if (models.get(i) instanceof Vehicle) {
+                Vehicle vehicle = (Vehicle) models.get(i);
+                message = counter + " " + "This Vehicle is a " + vehicle.getMakeModel() + " and it is worth " + vehicle.getValue();
+            }
+            System.out.println(message);
+        }
+
+        //Example 3
+        for (int i = 0; i < models.size(); i++) {
+            counter++;
+            if (models.get(i) instanceof House house) {
+                message = counter + " " + house.getDescription() + " is located at " + house.getAddress();
+            } else if (models.get(i) instanceof Vehicle vehicle) {
+                message = counter + " " + "This Vehicle is a " + vehicle.getMakeModel() + " and it is worth " + vehicle.getValue();
+            }
+
+
+            System.out.println(message);
+        }
+
+        // Example 4
+        for (Asset model : models) {
+            counter++;
+            if (model instanceof House) {
+                House house = (House) model;
+                message = counter + " " + house.getDescription() + " is located at " + house.getAddress();
+
+            } else if (model instanceof Vehicle) {
+                Vehicle vehicle = (Vehicle) model;
+                message = counter + " " + "This Vehicle is a " + vehicle.getMakeModel() + " and it is worth " + vehicle.getValue();
+            }
+            System.out.println(message);
+        }
+
+    }
+}
---
+++ src/main/java/com/pluralsight/models/Vehicle.java
@@ -0,0 +1,68 @@
+package com.pluralsight.models;
+
+import com.pluralsight.ui.Asset;
+
+public class Vehicle extends Asset {
+    private String makeModel;
+    private int year, odometer;
+
+    public Vehicle(String description, String dateAcquired, double originalCost, int odometer, int year, String makeModel) {
+        super(description, dateAcquired, originalCost);
+        this.odometer = odometer;
+        this.year = year;
+        this.makeModel = makeModel;
+    }
+
+    public Vehicle(String description, String dateAcquired, double originalCost) {
+        super(description, dateAcquired, originalCost);
+    }
+
+    public String getMakeModel() {
+        return makeModel;
+    }
+
+    public void setMakeModel(String makeModel) {
+        this.makeModel = makeModel;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public int getOdometer() {
+        return odometer;
+    }
+
+    public void setOdometer(int odometer) {
+        this.odometer = odometer;
+    }
+
+    @Override
+    public double getValue(){
+        double value = 0;
+
+
+        if(year >= 0 && year <= 3){
+            value = originalCost * (1 - 0.03 * year);
+        }else if(year >= 4 && year <= 6){
+            value = originalCost * (1 - 0.06 * year);
+        }else if(year >= 7 && year <= 10){
+            value = originalCost * (1 - 0.08 * year);
+        }else if(year > 10){
+            value = 1000.0;
+        }
+        if(odometer > 100000){
+
+        }
+        if (!makeModel.equalsIgnoreCase("honda") && !makeModel.equalsIgnoreCase("toyota")){
+        value = value * 0.75;
+
+        }
+
+       return  value;
+    }
+}
